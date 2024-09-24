3b8db8178e5a138e570754ee61211c5d8cdce0b6
William Taylor <wtaylor1984@gmail.com>
2024-09-23 21:22:00 -0700

    adding my changes.

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'William Taylor';
+export const siteTitle = 'Next.js Sample Website - Willy T';
 
 export default function Layout({ children, home }) {
   return (
@@ -20,7 +20,7 @@ export default function Layout({ children, home }) {
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -30,11 +30,11 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
-              alt={name}
+              alt=""
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
@@ -43,11 +43,11 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/profile.png"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
-                alt={name}
+                alt=""
               />
             </Link>
             <h2 className={utilStyles.headingLg}>
